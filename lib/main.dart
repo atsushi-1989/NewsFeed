eff279e873873e8b91f3a85dfec57d82a7bd9b70
tominagaatsushi <atsushitominaga5@gmail.com>
2021-01-26 02:28:27 +0900

    3章終了

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_feed_two/screens/home_screen.dart';
 import 'package:news_feed_two/style/style.dart';
 
 void main() => runApp(MyApp());
---
+++ lib/screens/pages/head_line_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HeadLinePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        //TODO
+        body: Container(
+          child: Center(
+            child: Text("headlinePage"),
+          ),
+        ),
+      ),
+    );
+  }
+}
